33113da6eada02d9b0672979760cfcdf1ac8c278
samanthakyra <samanthakyramendoza@gmail.com>
2022-11-19 00:12:22 -0800

    finished first asp

---
+++ Testing/Models/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testing.Models
+{
+    public interface IProductRepository
+    {
+
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
+
