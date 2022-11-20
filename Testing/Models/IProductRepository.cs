afa7ca631d9a791cdd106d1f5bf8e5db4ecdb274
samanthakyra <samanthakyramendoza@gmail.com>
2022-11-19 22:56:27 -0800

    completed create product

--- Testing/Models/IProductRepository.cs
+++ Testing/Models/IProductRepository.cs
@@ -11,6 +11,12 @@ namespace Testing.Models
         public Product GetProduct(int id);
 
         public void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+
+        public IEnumerable<Category> GetCategories();
+
+        public Product AssignCategory();
     }
 }
 
